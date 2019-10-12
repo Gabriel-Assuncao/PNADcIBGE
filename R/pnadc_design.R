03dbdfd9d7fce164892293ef79659db28bd7eecc
Gabriel Assuncao <pacotepnadc@ibge.gov.br>
2019-10-12 06:30:07 +0000

    version 0.5.0

--- R/pnadc_design.R
+++ R/pnadc_design.R
@@ -26,39 +26,45 @@
 #' @export
 
 pnadc_design <- function(data_pnadc){
-  options(survey.lonely.psu="adjust")
-  if("V1027" %in% names(data_pnadc)){
-    ########## creating desing object w/o poststratification
-    data_pre <- survey::svydesign(ids     = ~UPA,
-                        strata  = ~Estrato,
-                        data    = data_pnadc,
-                        weights = ~V1027,
-                        nest    = T)
-    ########## defining total for poststratification
-    popc.types <- data.frame(posest = as.character(unique(data_pnadc$posest)),
-                           Freq   = as.numeric(unique(data_pnadc$V1029)))
-    popc.types <- (popc.types[order(popc.types$posest),])
-    ########## creating final desing object w/ poststratification
-    data_pos <- survey::postStratify(design     = data_pre,
-                           strata     = ~posest,
-                           population = popc.types)
+  if(sum(class(data_pnadc) == "tbl_df") > 0){
+    options(survey.lonely.psu="adjust")
+    if("V1027" %in% names(data_pnadc)){
+      ########## creating desing object w/o poststratification
+      data_pre <- survey::svydesign(ids     = ~UPA,
+                          strata  = ~Estrato,
+                          data    = data_pnadc,
+                          weights = ~V1027,
+                          nest    = T)
+      ########## defining total for poststratification
+      popc.types <- data.frame(posest = as.character(unique(data_pnadc$posest)),
+                             Freq   = as.numeric(unique(data_pnadc$V1029)))
+      popc.types <- (popc.types[order(popc.types$posest),])
+      ########## creating final desing object w/ poststratification
+      data_pos <- survey::postStratify(design     = data_pre,
+                             strata     = ~posest,
+                             population = popc.types)
+    }
+    else{
+      ########## creating desing object w/o poststratification
+      data_pre <- survey::svydesign(ids     = ~UPA,
+                                    strata  = ~Estrato,
+                                    data    = data_pnadc,
+                                    weights = ~V1031,
+                                    nest    = T)
+      ########## defining total for poststratification
+      popc.types <- data.frame(posest = as.character(unique(data_pnadc$posest)),
+                               Freq   = as.numeric(unique(data_pnadc$V1030)))
+      popc.types <- (popc.types[order(popc.types$posest),])
+      ########## creating final desing object w/ poststratification
+      data_pos <- survey::postStratify(design     = data_pre,
+                                       strata     = ~posest,
+                                       population = popc.types)
+  
+    }
   }
   else{
-    ########## creating desing object w/o poststratification
-    data_pre <- survey::svydesign(ids     = ~UPA,
-                                  strata  = ~Estrato,
-                                  data    = data_pnadc,
-                                  weights = ~V1031,
-                                  nest    = T)
-    ########## defining total for poststratification
-    popc.types <- data.frame(posest = as.character(unique(data_pnadc$posest)),
-                             Freq   = as.numeric(unique(data_pnadc$V1030)))
-    popc.types <- (popc.types[order(popc.types$posest),])
-    ########## creating final desing object w/ poststratification
-    data_pos <- survey::postStratify(design     = data_pre,
-                                     strata     = ~posest,
-                                     population = popc.types)
-
+    warning("The sample design was already defined for this microdata.")
+    data_pos <- data_pnadc
   }
-return(data_pos)
+  return(data_pos)
 }
--- R/pnadc_labeller.R
+++ R/pnadc_labeller.R
@@ -14,39 +14,44 @@
 #' @export
 
 pnadc_labeller <- function(data_pnadc,dictionary.file){
-  dictionary <- readxl::read_excel(dictionary.file)
-  X__3=X__6=X__7=NULL
-  colnames(dictionary) <- paste0("X__",1:dim(dictionary)[2])
-  dictionary %<>% subset(!is.na(X__6))
-  codcurrent <- dictionary$X__3
-  for(i in 1:dim(dictionary)[1]){
-    if(is.na(dictionary$X__3[i])){
-      dictionary$X__3[i] <- codcurrent
+  if(sum(class(data_pnadc) == "tbl_df") > 0){
+    dictionary <- suppressMessages(readxl::read_excel(dictionary.file))
+    X__3=X__6=X__7=NULL
+    colnames(dictionary) <- paste0("X__",1:dim(dictionary)[2])
+    dictionary %<>% subset(!is.na(X__6))
+    codcurrent <- dictionary$X__3
+    for(i in 1:dim(dictionary)[1]){
+      if(is.na(dictionary$X__3[i])){
+        dictionary$X__3[i] <- codcurrent
+      }
+      else{
+        codcurrent <- dictionary$X__3[i]
+      }
     }
-    else{
-      codcurrent <- dictionary$X__3[i]
+    notlabel <- c("Ano", "Trimestre", "UPA", "Estrato", "V1008",
+                  "V1014", "V1016", "posest", "V2003", "V2008", "V20081",
+                  "V20082", "V40081", "V40082", "V40083", "V4010", "V4013",
+                  "V4041", "V4044","V4075A1","VD4031","VD4035",
+                  "V401511","V401512","V40161","V40162","V40163","V401711",
+                  "V40181","V40182","V40183")
+    vars <- names(data_pnadc)
+    varsc <- vars[sapply(data_pnadc, class) == "character"]
+    varsf <- setdiff(varsc, notlabel)
+    for (i in 1:length(varsf)) {
+      if (i > 0 && varsf[i] %in% (dictionary$X__3)) {
+        data_pnadc[varsf[i]] <- factor(suppressWarnings(as.numeric(unlist(data_pnadc[varsf[i]]))),
+                                       levels = suppressWarnings(
+                                         as.numeric(unlist(dictionary %>%
+                                                subset(X__3 == varsf[i]) %>%
+                                                select(X__6)))),
+                                       labels = unlist(dictionary %>%
+                                                subset(X__3 == varsf[i]) %>%
+                                                select(X__7)))
+      }
     }
   }
-  notlabel <- c("Ano", "Trimestre", "UPA", "Estrato", "V1008",
-                "V1014", "V1016", "posest", "V2003", "V2008", "V20081",
-                "V20082", "V40081", "V40082", "V40083", "V4010", "V4013",
-                "V4041", "V4044","V4075A1","VD4031","VD4035",
-                "V401511","V401512","V40161","V40162","V40163","V401711",
-                "V40181","V40182","V40183")
-  vars <- names(data_pnadc)
-  varsc <- vars[sapply(data_pnadc, class) == "character"]
-  varsf <- setdiff(varsc, notlabel)
-  for (i in 1:length(varsf)) {
-    if (varsf[i] %in% (dictionary$X__3)) {
-      data_pnadc[varsf[i]] <- factor(as.numeric(unlist(data_pnadc[varsf[i]])),
-                                     levels = suppressWarnings(
-                                       as.numeric(unlist(dictionary %>%
-                                              subset(X__3 == varsf[i]) %>%
-                                              select(X__6)))),
-                                     labels = unlist(dictionary %>%
-                                              subset(X__3 == varsf[i]) %>%
-                                              select(X__7)))
-    }
+  else{
+    warning("The sample design was already defined for this microdata.")
   }
   return(data_pnadc)
 }
