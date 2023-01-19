1251b6d64396088bfc03bc159518ec25f37fd0a3
Gabriel Henrique Oliveira Assuncao <gabriel.assuncao@ibge.gov.br>
2023-01-18 23:11:56 -0300

    A detailed review was performed in packages and instances were created to avoid errors in cases where the reference microdata is not yet available.

--- R/pnadc_deflator.R
+++ R/pnadc_deflator.R
@@ -17,8 +17,9 @@
 #' pnadc.df <- pnadc_deflator(data_pnadc=pnadc.df, deflator.file=deflator.path)
 #' \donttest{
 #' # Downloading data
-#' pnadc.df2 <- get_pnadc(year=2017, quarter=4, vars=c("VD4001","VD4002"), defyear=2017, defperiod=4,
-#'                       labels=TRUE, deflator=FALSE, design=FALSE, savedir=tempdir())
+#' pnadc.df2 <- get_pnadc(year=2017, quarter=4, selected=FALSE, vars=c("VD4001","VD4002"),
+#'                        defyear=2017, defperiod=4, labels=TRUE, deflator=FALSE, design=FALSE,
+#'                        savedir=tempdir())
 #' deflator.path2 <- pnadc_example(path="deflatorexample.xls")
 #' pnadc.df2 <- pnadc_deflator(data_pnadc=pnadc.df2, deflator.file=deflator.path2)}
 #' @export
